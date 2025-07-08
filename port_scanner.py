8eeeee2c857c406568d40370cfd4bf0a818031c3
Kaushal Sule <114328847+KAUZHAL@users.noreply.github.com>
2025-07-09 01:06:49 +0530

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,22 +1,25 @@
 import socket
-import subprocess
-from _datetime import datetime
-target=int("Enter the target IP Address")
+from datetime import datetime
+
+target = input("Enter the target IP Address or hostname: ")
+
 def port_scan(target):
     try:
-        ip=socket.gethostname(target)
+        ip = socket.gethostbyname(target)
         print(f'Scanning the target {ip}')
-        print(f'Time started:',datetime.now())
-        for port in range(20,90):
-            sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        print('Time started:', datetime.now())
+
+        for port in range(20, 90):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(1)
-            result=sock.connect_ex((ip,port))
-            if result==0:
-                print("Port{}:Open".format(port))
+            result = sock.connect_ex((ip, port))
+            if result == 0:
+                print(f"Port {port}: Open")
             sock.close()
+
     except socket.gaierror:
-        print("Hostname could not be resolved")
+        print("Hostname could not be resolved.")
     except socket.error:
-        print("Could not connect to the server")
+        print("Could not connect to the server.")
 
 port_scan(target)
